54d1dbe73a498da403a40412ff00400bc0c75147
Michael Bauer <mihi@lo-res.org>
2013-12-20 14:09:15 +0100

    added badger urls

--- goad/urls.py
+++ goad/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import badger.urls_simple 
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'goad.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include(badger.urls_simple)),
     url(r'^admin/', include(admin.site.urls)),
 )
