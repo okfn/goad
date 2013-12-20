2ed0dd0e9f31f55da42fabf171ed4f0020c5e13d
Michael Bauer <mihi@lo-res.org>
2013-12-20 11:09:04 +0100

    added the badger service

--- goad/settings.py
+++ goad/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'badger'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =  (
   os.path.join(BASE_DIR, 'static' ),
    )
+
+BADGER_SITE_ISSUER= {
+ 'origin': 'http://okfn.org',
+ 'name': 'Goad',
+ 'org': 'Open Knowledge Foundation',
+ 'contact': 'michael.bauer@okfn.org',
+                }
